44bd21e9a308dc3d4c5f092603ecbf3ac9627c58
VeronikaPolkhanova <halky.boom@gmail.com>
2022-07-17 22:35:19 +0300

    added arrow back in description item

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -1,5 +1,7 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi'
 
 import VideoBg from '../videoBg';
 import FavStar from '../favStar';
@@ -16,6 +18,9 @@ function Description() {
         <div className="description">
             <VideoBg url="https://cdn.videvo.net/videvo_files/video/free/2013-10/large_watermarked/Background_08_preview.mp4" />
             <div className="item">
+                <Link to='/gallery'>
+                    <HiOutlineArrowNarrowLeft className='back-link' />
+                </Link>
                 <img src={state.url} alt={state.id} />
                 <div className="description-text">
                     <p className="title">{state.title}</p>
